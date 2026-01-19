d04294c699b5d13216ec5ef779297b0233ac0461
ayush <savaliayaayush2295@gmail.com>
2026-01-19 10:46:15 +0530

    Add: Api basic setup

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    dd('welcome');
+    return $request->user();
+});
